7374f1e921ae119e8030cbbd00070444d145020c
ksilverstone <kerememre4141@gmail.com>
2026-04-04 14:20:48 +0300

    User.java dosyası eklendi

---
+++ user-service/src/main/java/yazlab/domain/User.java
@@ -0,0 +1,20 @@
+package yazlab.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
